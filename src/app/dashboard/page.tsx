aafc89e235317ce1a7d05185fbf3ddea36092cbf
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-12-01 18:59:33 +0530

    [structure fix]

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,12 +3,15 @@ import { Navbar } from "@/components/navigation/navbar/page";
 import React from "react";
 
 function page() {
+  // console.log("Clerk Publishable Key:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+
   return (
     <div>
       <Navbar />
       <section className="max-w-[1200px] mx-auto items-center justify-center py-20 flex flex-col md:py-22">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-light tracking-tight  ">Choose a template and start creating</h1>
         <div className="mt-10"><SearchBar /></div>
+        
       </section>
     </div>
   );
