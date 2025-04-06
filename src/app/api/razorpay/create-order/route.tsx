7807a48f8af3a2cb5980bd2b805dbfd2b6ccb467
Ankit-Raj27 <ankitrajk27@gmail.com>
2025-04-07 02:42:05 +0530

    [vercel deployment error]

--- src/app/api/razorpay/create-order/route.tsx
+++ src/app/api/razorpay/create-order/route.tsx
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 import { auth } from '@clerk/nextjs/server'
 
 const razorpay = new Razorpay({
-  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
+  key_id: process.env.RAZORPAY_KEY_ID!,
   key_secret: process.env.RAZORPAY_KEY_SECRET!,
 })
 export async function POST(req: Request) {
