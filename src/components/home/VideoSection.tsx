c98f3af06a6c71dabd47e8c09000f1ba5d68086e
Ankit-Raj27 <ankitrajk27@gmail.com>
2025-04-30 16:41:57 +0530

    [video player fix]

--- src/components/home/VideoSection.tsx
+++ src/components/home/VideoSection.tsx
@@ -94,7 +94,7 @@ export default function VideoPlayer({ src, poster }: VideoPlayerProps) {
                         playsInline
                         preload="metadata"
                     >
-                        <source src={src} type="video/mp4" />
+                        <source src={src} type="/public/Demo.mp4" />
                         Your browser does not support the video tag.
                     </video>
                 </div>
